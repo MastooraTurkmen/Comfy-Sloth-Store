6d6b127f1eda059932013e09fc176110ec5eadc5
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-23 18:20:27 +0430

    set up

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,7 +9,30 @@ import CartButtons from './CartButtons'
 import { useUserContext } from '../context/user_context'
 
 const Sidebar = () => {
-  return <h4>sidebar</h4>
+  const isOpen = false;
+
+  return <SidebarContainer>
+    <aside className={`${isOpen ? 'sidebar show-sidebar' : 'sidebar'}`}>
+      <div className="sidebar-header">
+        <img src={logo} alt="logo" className="logo" />
+        <button type="button" className='close-btn '>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map(({ id, text, url }) => {
+          return (
+            <li key={id}>
+              <Link to={url}>{text}</Link>
+            </li>
+          )
+        })}
+        <li>
+          <Link to='/chekout'>chekout</Link>
+        </li>
+      </ul>
+    </aside>
+  </SidebarContainer>
 }
 
 const SidebarContainer = styled.div`
