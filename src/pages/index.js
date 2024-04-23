a56e8974bcc92d47145f086cabd53c45d19f3c4f
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-23 11:04:43 +0430

    import and export components

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from './HomePage'
+import About from './AboutPage'
+import Error from './ErrorPage'
+import Cart from './CartPage'
+import PrivateRoute from './PrivateRoute'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import Checkout from './CheckoutPage'
+
+export { Home, About, Error, Cart, PrivateRoute, Products, SingleProduct, Checkout }
