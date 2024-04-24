c4d0159643c9b52164366095ca2c6aa097652394
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-24 15:14:54 +0430

    products provider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer, Dashboard } from './components'
+import { Navbar, Sidebar, Footer } from './components'
 import { Home, About, Error, Cart, PrivateRoute, Products, SingleProduct, Checkout } from './pages/index'
 
 
