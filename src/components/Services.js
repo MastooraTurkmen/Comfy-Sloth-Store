6509e083ff1599ae3052c3c1e8c82a9be63b6475
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-24 17:35:01 +0430

    services js

--- src/components/Services.js
+++ src/components/Services.js
@@ -4,7 +4,27 @@ import { services } from '../utils/constants'
 
 const Services = () => {
   return (
-    <Wrapper></Wrapper>
+    <Wrapper>
+      <div className="section-center">
+        <article className="header">
+          <h3>custom furniture <br /> built only for you</h3>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Facere maiores mollitia aut soluta molestias porro,
+            cum in quaerat obcaecati dolores voluptatem delectus ipsa,
+            beatae ducimus quia dignissimos architecto nobis officiis.</p>
+        </article>
+        <div className="services-center">
+          {services.map((service) => {
+            const { id, icon, title, text } = service
+            return <article key={id} className='service'>
+              <span className="icon">{icon}</span>
+              <h4>{title}</h4>
+              <p>{text}</p>
+            </article>
+          })}
+        </div>
+      </div>
+    </Wrapper>
   )
 }
 
