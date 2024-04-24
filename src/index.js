966499ff62515d6a2ee01b7d76fea7f20ac08049
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-24 15:14:50 +0430

    products provider

--- src/index.js
+++ src/index.js
@@ -11,4 +11,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+root.render(
+    <ProductsProvider>
+        <App />
+    </ProductsProvider>
+);
