7cfa0c85a20b09d57eee42b16bb9ede2a261266f
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-24 16:40:02 +0430

    about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,30 @@ import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return (
+    <main>
+      <PageHero title='about' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Ut earum, odit laboriosam quidem est sapiente optio esse saepe!
+            Atque dolores fugiat reprehenderit architecto. Odit error ex atque hic suscipit labore!
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Ut earum, odit laboriosam quidem est sapiente optio esse saepe!
+            Atque dolores fugiat reprehenderit architecto. Odit error ex atque hic suscipit labore!
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Ut earum, odit laboriosam quidem est sapiente optio esse saepe!
+            Atque dolores fugiat reprehenderit architecto. Odit error ex atque hic suscipit labore!
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  )
 }
 
 const Wrapper = styled.section`
