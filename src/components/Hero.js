fe889ca6a28c64962230a4c9292dc1e47d302569
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-24 17:06:22 +0430

    hero page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,22 @@ import heroBcg from '../assets/hero-bcg.jpeg'
 import heroBcg2 from '../assets/hero-bcg-2.jpeg'
 
 const Hero = () => {
-  return <h4>hero</h4>
+  return (
+    <Wrapper className='section-center'>
+      <article className="content">
+        <h1>design your<br /> comfort zone</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Distinctio culpa ea quisquam atque ipsa! Eius perspiciatis ratione beatae
+          ut vero consectetur praesentium similique quos excepturi error, adipisci, hic, voluptates tenetur.
+        </p>
+        <Link to='/products' className='btn hero-btn'>shop now</Link>
+      </article>
+      <article className="img-container">
+        <img src={heroBcg} alt="nice table" className='main-img' />
+        <img src={heroBcg2} alt="person working" className='accent-img' />
+      </article>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
