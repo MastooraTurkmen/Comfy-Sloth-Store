2a132b3d6074f70752611bcc179257a76f4599e3
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-25 09:58:05 +0430

    products content

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -37,8 +37,14 @@ export const ProductsProvider = ({ children }) => {
   }
 
   const fetchProducts = async (url) => {
-    const response = await axios.get(url)
-    console.log(response);
+    dispatch({ type: GET_PRODUCTS_BEGIN })
+    try {
+      const response = await axios.get(url)
+      const products = response.data
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products })
+    } catch (error) {
+      dispatch({ type: GET_PRODUCTS_ERROR })
+    }
   }
 
   useEffect(() => {
