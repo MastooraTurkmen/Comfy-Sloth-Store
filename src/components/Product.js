82400165851d830e391801f98c10d947d77d81b3
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-25 11:11:43 +0430

    format price

--- src/components/Product.js
+++ src/components/Product.js
@@ -15,7 +15,7 @@ const Product = ({ name, image, price, id }) => {
       </div>
       <footer>
         <h5>{name}</h5>
-        <p>{price}</p>
+        <p>{formatPrice(price)}</p>
       </footer>
     </Wrapper>
   )
