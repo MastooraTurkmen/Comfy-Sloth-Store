d3f93cb84c806daf00e3d6854918a1c3882ed5b3
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-25 10:32:29 +0430

    get all featured product

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -7,7 +7,29 @@ import Loading from './Loading'
 import Product from './Product'
 
 const FeaturedProducts = () => {
-  return <h4>featured products</h4>
+  const { products_loading: loading, featured_products: products, products_error: error } = useProductsContext()
+
+  if (loading) {
+    return <Loading />
+  }
+  if (error) {
+    return <Error />
+  }
+
+  return (
+    <Wrapper className='section'>
+      <div className="title">
+        <h2>featured products</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="section-center featured">
+        {products.slice(2, 5).map((product) => {
+          console.log(product);
+          return <Product key={product.id} {...product} />
+        })}
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
