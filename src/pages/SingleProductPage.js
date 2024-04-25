f7a45c889171432a0bfea134df44128ea5eb9d16
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-25 14:48:26 +0430

    single product

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { useParams, useHistory } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { useProductsContext } from '../context/products_context'
 import { single_product_url as url } from '../utils/constants'
 import { formatPrice } from '../utils/helpers'
@@ -15,7 +15,22 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 const SingleProductPage = () => {
-  return <h4>single product page</h4>
+  const { id } = useParams();
+  const navigate = useNavigate()
+  const { single_product_loading: loading, single_product_error: error,
+    single_product: product, fetchSingleProduct } = useProductsContext()
+
+  useEffect(() => {
+    fetchSingleProduct(`${url}${id}`)
+  }, [id])
+
+  console.log(product);
+
+  return (
+    <Wrapper>
+      <h4>single product page</h4>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.main`
