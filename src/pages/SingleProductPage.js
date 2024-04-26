0fbf4c826857ce8529be2b29c2dd002c4ebbca67
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-26 10:58:40 +0430

    single Product image

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -49,7 +49,7 @@ const SingleProductPage = () => {
       <div className="section section-center page">
         <Link to='/products' className='btn'>back to products</Link>
         <div className="product-center">
-          <ProductImages />
+          <ProductImages images={images} />
           <section className="content">
             <h2>{name}</h2>
             <Stars />
