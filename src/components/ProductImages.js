a80dd4d747df47e6c9d182f6ef678fc623813fa2
MastooraTurkmen <janmastoorajan@gmail.com>
2024-04-26 10:58:33 +0430

    Product image

--- src/components/ProductImages.js
+++ src/components/ProductImages.js
@@ -1,8 +1,26 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 
-const ProductImages = () => {
-  return <h4>product images</h4>
+const ProductImages = ({ images = [{ url: '' }] }) => {
+  const [main, setMain] = useState(images[0])
+
+  return (
+    <Wrapper>
+      <img className='main' src={main.url} alt='main image' />
+      <div className="gallery">
+        {images.map((image, index) => {
+          console.log(image);
+          return <img
+            key={index}
+            onClick={() => setMain(images[index])}
+            alt={image.filename}
+            src={image.url}
+            className={`${image.url === main.url ? 'active' : null}`}
+          />
+        })}
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
