b79855b56f9956403a4e2b05813c58e59ba6e75e
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-05 08:55:41 +0430

    set up add to cart

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -5,8 +5,32 @@ import { FaCheck } from 'react-icons/fa'
 import { useCartContext } from '../context/cart_context'
 import AmountButtons from './AmountButtons'
 
-const AddToCart = () => {
-  return <h4>addToCart </h4>
+const AddToCart = ({ product }) => {
+  const { id, stock, colors } = product
+  const [mainColor, setMainColor] = useState(colors[0])
+
+  return (
+    <Wrapper>
+      <div className="colors">
+        <span>colors: </span>
+        <div>{
+          colors.map((color, index) => {
+            return (
+              <button
+                key={index}
+                style={{ background: color }}
+                className={`${mainColor === color ? 'color-btn active' : 'color-btn'}`}
+                onChange={() => setMainColor(color)}
+              >
+                {mainColor === color ? <FaCheck /> : null}
+              </button>
+            )
+          })
+        }</div>
+      </div>
+      <div className="btn-container"></div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
