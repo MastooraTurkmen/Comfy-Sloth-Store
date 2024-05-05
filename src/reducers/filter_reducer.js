9881301e695336740b9d4b5ec277c9aeb5c766ce
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-05 19:10:32 +0430

    filter context

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -13,6 +13,14 @@ const filter_reducer = (state, action) => {
   if (action.type === LOAD_PRODUCTS) {
     return { ...state, all_products: [...action.payload], filtered_products: [...action.payload] }
   }
+
+  if (action.type === SET_GRIDVIEW) {
+    return { ...state, grid_view: true }
+  }
+  if (action.type === SET_LISTVIEW) {
+    return { ...state, grid_view: false }
+  }
+
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 
