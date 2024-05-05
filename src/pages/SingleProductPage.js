ce7cf00432108c676021924b4af5ca311765b05a
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-05 08:24:55 +0430

    set up stars

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -52,7 +52,7 @@ const SingleProductPage = () => {
           <ProductImages images={images} />
           <section className="content">
             <h2>{name}</h2>
-            <Stars />
+            <Stars stars={stars} reviews={reviews} />
             <h5>{formatPrice(price)}</h5>
             <p className='description'>{description}</p>
             <p className="info">
