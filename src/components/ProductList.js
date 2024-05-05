d17b537a175b8cf633c4259877c02c3129b8ad30
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-05 18:08:38 +0430

    product list

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,7 +4,18 @@ import GridView from './GridView'
 import ListView from './ListView'
 
 const ProductList = () => {
-  const { filtered_products: products } = useFilterContext();
+  const { filtered_products: products, grid_view } = useFilterContext();
+
+  if (products.length < 1) {
+    return (
+      <h5 style={{ textTransform: 'none' }}>sorry, no products matched your search</h5>
+    )
+  }
+
+  if (grid_view === false) {
+    return <ListView products={products} />
+  }
+
   return (
     <GridView products={products}>product list</GridView>
   )
