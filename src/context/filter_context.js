07b0994ad9153fda05356befeabe7c6d241fa7d1
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-05 10:55:08 +0430

    products

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -20,7 +20,12 @@ const initialState = {
 const FilterContext = React.createContext()
 
 export const FilterProvider = ({ children }) => {
+  const { products } = useProductsContext()
   const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    dispatch({ type: LOAD_PRODUCTS, payload: products })
+  }, [])
 
   return (
     <FilterContext.Provider value='filter context'>
