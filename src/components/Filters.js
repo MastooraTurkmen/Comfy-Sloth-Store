36f87ae8d71caff57db8e37bb864b958eb702e1e
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-06 17:39:38 +0430

    filter search product

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -5,7 +5,32 @@ import { getUniqueValues, formatPrice } from '../utils/helpers'
 import { FaCheck } from 'react-icons/fa'
 
 const Filters = () => {
-  return <h4>filters</h4>
+  const {
+    filters: {
+      text, category, company, color, price, max_price, shipping
+    },
+    updateFilters,
+    clearFilters,
+    all_products
+  } = useFilterContext();
+
+  return <Wrapper>
+    <div className="content">
+      <form onSubmit={(e) => e.preventDefault()}></form>
+      {/* search input */}
+      <div className="form-control">
+        <input
+          type="text"
+          name="text"
+          placeholder='search'
+          value={text}
+          className='search-input'
+          onChange={updateFilters}
+        />
+      </div>
+      {/* end search input */}
+    </div>
+  </Wrapper>
 }
 
 const Wrapper = styled.section`
