103cf8f3e55686447c336ba08111a5e380cfec4c
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-06 21:23:15 +0430

    filter search product

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -7,5 +7,8 @@ export const formatPrice = (number) => {
 
 export const getUniqueValues = (data, type) => {
     let unique = data.map((item) => item[type])
+    if (type === 'colors') {
+        unique = unique.flat()
+    }
     return ['all', ...new Set(unique)]
 }
