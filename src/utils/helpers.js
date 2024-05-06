66efeaf0b1d7d8553e0a8096b103b01aadc2b9d7
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-06 21:19:27 +0430

    filter search product

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5,4 +5,7 @@ export const formatPrice = (number) => {
 
 }
 
-export const getUniqueValues = () => { }
+export const getUniqueValues = (data, type) => {
+    let unique = data.map((item) => item[type])
+    return ['all', ...new Set(unique)]
+}
