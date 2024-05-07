bf7a0c5caacac907c950488de9dc42bda8d188a0
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-07 15:14:36 +0430

    set up clear filter

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -123,6 +123,7 @@ const Filters = () => {
         </div>
         {/* end of shipping */}
       </form>
+      <button type="button" className='clear-btn' onClick={clearFilters}>clear filters</button>
     </div>
   </Wrapper >
 }
