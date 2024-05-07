447b4a22b56ee1b68985fddbaa33356c6eff07e1
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-07 15:11:54 +0430

    shipping

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -71,6 +71,9 @@ export const FilterProvider = ({ children }) => {
     if (name === 'price') {
       value = Number(value)
     }
+    if (name === 'shipping') {
+      value = e.target.checked
+    }
     dispatch({ type: UPDATE_FILTERS, payload: { name, value } })
   }
   const clearFilters = (e) => { }
