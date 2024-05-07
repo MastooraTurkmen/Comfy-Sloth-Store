7b71816f86eeb7b1d66363289feccdc0270e91dd
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-07 13:11:29 +0430

    set up colors

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -65,6 +65,9 @@ export const FilterProvider = ({ children }) => {
     if (name === 'category') {
       value = e.target.textContent
     }
+    if (name === 'color') {
+      value = e.target.dataset.color
+    }
     dispatch({ type: UPDATE_FILTERS, payload: { name, value } })
   }
   const clearFilters = (e) => { }
