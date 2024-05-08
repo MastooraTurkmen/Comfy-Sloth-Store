94070d68212380cbd63dfd92e0c6d84a6836da47
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-08 20:05:34 +0430

    cart item

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -40,7 +40,9 @@ export const CartProvider = ({ children }) => {
   }
 
   // toggle amount
-  const toggleAmount = (id, value) => { }
+  const toggleAmount = (id, value) => {
+    dispatch({ type: TOGGLE_CART_ITEM_AMOUNT, payload: { id, value } })
+  }
 
   // clear cart
   const clearCart = () => { }
