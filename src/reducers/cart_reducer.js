2a9a74506888ad14bd126d92271600e45a63ec3f
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-09 20:42:07 +0430

    set up

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -71,6 +71,23 @@ const cart_reducer = (state, action) => {
         return item
       }
     })
+
+    return { ...state, cart: tempCart }
+  }
+
+  if (action.type === COUNT_CART_TOTALS) {
+    const { total_items, total_amount } = state.cart.reduce((total, cartItem) => {
+      const { amount, price } = cartItem
+
+      total.total_items += amount;
+      total.total_amount += price * amount;
+
+      return total
+    }, {
+      total_items: 0,
+      total_amount: 0
+    })
+    return { ...state, total_items, total_amount }
   }
 
   throw new Error(`No Matching "${action.type}" - action type`)
