c1dcd24c7200749a726fed59194f083dc95c7603
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-11 18:56:01 +0430

    cart button js

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -8,7 +8,7 @@ import { useUserContext } from '../context/user_context'
 
 const CartButtons = () => {
   const { closeSidebar } = useProductsContext()
-  const { total_items } = useCartContext()
+  const { total_items, clearCart } = useCartContext()
   const { loginWithRedirect, logout, myUser } = useUserContext()
 
   return <Wrapper>
@@ -20,13 +20,15 @@ const CartButtons = () => {
       </span>
     </Link>
     {myUser ? (
-
       <button type="button" className='auth-btn' onClick={loginWithRedirect}>
         login <FaUserPlus />
       </button>
     ) : (
-
-      <button type="button" className='auth-btn' onClick={() => logout({ returnTo: window.location.origin })}>
+      <button type="button" className='auth-btn' onClick={() => {
+        clearCart()
+        logout({ returnTo: window.location.origin })
+      }
+      }>
         logout <FaUserMinus />
       </button>
     )}
