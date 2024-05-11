f227f8128faca39b0ba094ede3f52cf56fe70558
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-11 08:53:20 +0430

    auth

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
-import { Home, About, Error, Cart, PrivateRoute, Products, SingleProduct, Checkout } from './pages/index'
+import { Home, About, Error, Cart, PrivateRoute, Products, SingleProduct, Checkout, AuthWrapper } from './pages/index'
 
 
 function App() {
 
   return (
-    <Router>
-      <Navbar />
-      <Sidebar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="cart" element={<Cart />} />
-        <PrivateRoute path="checkout" element={<Checkout />} />
-        <Route path="products" element={<Products />} />
-        <Route path="products/:id" element={<SingleProduct />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="cart" element={<Cart />} />
+          <PrivateRoute path="checkout" element={<Checkout />} />
+          <Route path="products" element={<Products />} />
+          <Route path="products/:id" element={<SingleProduct />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </AuthWrapper>
   )
 }
 
