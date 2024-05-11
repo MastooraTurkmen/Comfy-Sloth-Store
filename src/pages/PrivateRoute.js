ede7e84955404de679642ee5bc5fad694dd05749
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-11 08:54:00 +0430

    private route

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Route, Navigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
-// will remove later
-import { useUserContext } from '../context/user_context';
 
 const PrivateRoute = ({ children, ...rest }) => {
-  const { myUser } = useUserContext()
+  const { user } = useAuth0()
 
-  return <Route {...rest} render={() => {
-    return myUser ? children : <Navigate to='/' />
-  }}></Route>
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        return user ? children : <Navigate to='/' />
+      }}></Route>
+  )
 };
 export default PrivateRoute;
