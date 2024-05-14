ce20d334dc23c7f1e158486c5d600d693b717030
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-14 08:02:55 +0430

    set up create payment intent

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -1,10 +1,15 @@
 // domain/netlify/function/create-payment-intent
 
 exports.handler = async function (event, context) {
-    const { cart, shipping_fee, total_amount } = JSON.parse(event.body)
-
+    if (event.body) {
+        const { cart, shipping_fee, total_amount } = JSON.parse(event.body)
+        return {
+            statusCode: 200,
+            body: JSON.stringify(cart),
+        }
+    }
     return {
         statusCode: 200,
-        body: 'payment intent',
+        body: "Create Payment Intent",
     }
 }
