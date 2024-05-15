8db84086ab9737e99769c2f716c2f98ce92c622d
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-15 10:53:24 +0430

    set up

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
 import { ProductsProvider } from './context/products_context';
 import { FilterProvider } from './context/filter_context';
 import { CartProvider } from './context/cart_context';
@@ -10,15 +9,13 @@ import { UserProvider } from './context/user_context';
 import { Auth0Provider } from '@auth0/auth0-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
 root.render(
     <Auth0Provider
         domain={process.env.REACT_APP_AUTH_DOMAIN}
         clientId={process.env.REACT_APP_CLIENT_ID}
         authorizationParams={{
-            redirect_uri: window.location.origin
+            redirect_uri: `${window.location.origin}/cart`,
         }}
-        cacheLocation="localStorage"
     >
         <UserProvider>
             <ProductsProvider>
