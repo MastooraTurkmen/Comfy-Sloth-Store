6d35881b1dcc4920754bfbd6517fe1aa5b92ae7b
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-15 10:55:19 +0430

    set up

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -4,29 +4,46 @@ import { BsFillGridFill, BsList } from 'react-icons/bs'
 import styled from 'styled-components'
 
 const Sort = () => {
-  const { filtered_products: products, grid_view, setListView, setGridView, sort, updateSort } = useFilterContext();
+  const {
+    filtered_products: products,
+    grid_view,
+    setGridView,
+    setListView,
+    sort,
+    updateSort,
+  } = useFilterContext()
 
   return (
     <Wrapper>
-      <div className="btn-container">
-        <button type="button" className={`${grid_view ? 'active' : null}`} onClick={setGridView}>
+      <div className='btn-container'>
+        <button
+          onClick={setGridView}
+          className={`${grid_view ? 'active' : null}`}
+        >
           <BsFillGridFill />
         </button>
-        <button type="button" className={`${!grid_view ? 'active' : null}`} onClick={setListView}>
+        <button
+          onClick={setListView}
+          className={`${!grid_view ? 'active' : null}`}
+        >
           <BsList />
         </button>
       </div>
-      <p>
-        {products.length} products found
-      </p>
+      <p>{products.length} products found</p>
       <hr />
       <form>
-        <label htmlFor="sort">sort by</label>
-        <select value={sort} onChange={updateSort} name="sort" id="sort" className='sort-input'>
-          <option value="price-lowest">price (lowest)</option>
-          <option value="price-highest">price (highest)</option>
-          <option value="name-a">name (a-z)</option>
-          <option value="name-z">name (z-a)</option>
+        <label htmlFor='sort'>sort by</label>
+        <select
+          name='sort'
+          id='sort'
+          value={sort}
+          onChange={updateSort}
+          className='sort-input'
+        >
+          <option value='price-lowest'>price (lowest)</option>
+          <option value='price-highest'>price (highest)</option>
+          <option value='name-a'>name (a - z)</option>
+          <option value='name-z'>name (z - a)</option>
         </select>
       </form>
     </Wrapper>
