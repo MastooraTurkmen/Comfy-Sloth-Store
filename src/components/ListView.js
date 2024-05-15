f23a0faef39d60fadf5d46adcf043ac8da17c302
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-15 10:54:53 +0430

    set up

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -8,15 +8,19 @@ const ListView = ({ products }) => {
     <Wrapper>
       {products.map((product) => {
         const { id, image, name, price, description } = product
-        return <article key={id}>
-          <img src={image} alt={name} />
-          <div>
-            <h4>{name}</h4>
-            <h5 className='price'>{formatPrice(price)}</h5>
-            <p>{description.substring(0, 150)}...</p>
-            <Link to={`/products/${id}`} className='btn'>Details</Link>
-          </div>
-        </article>
+        return (
+          <article key={id}>
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <h5 className='price'>{formatPrice(price)}</h5>
+              <p>{description.substring(0, 150)}...</p>
+              <Link to={`/products/${id}`} className='btn'>
+                Details
+              </Link>
+            </div>
+          </article>
+        )
       })}
     </Wrapper>
   )
