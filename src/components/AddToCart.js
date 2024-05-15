fde98ac6895f0078658842123858f559009573d2
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-15 10:53:28 +0430

    set up

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -6,24 +6,25 @@ import { useCartContext } from '../context/cart_context'
 import AmountButtons from './AmountButtons'
 
 const AddToCart = ({ product }) => {
+  // add to cart
   const { addToCart } = useCartContext()
   const { id, stock, colors } = product
-
   const [mainColor, setMainColor] = useState(colors[0])
-  const [amount, setAmount] = useState(0)
+  const [amount, setAmount] = useState(1)
 
   const increase = () => {
     setAmount((oldAmount) => {
-      let tempAmount = oldAmount + 1;
+      let tempAmount = oldAmount + 1
       if (tempAmount > stock) {
         tempAmount = stock
       }
       return tempAmount
     })
   }
+  
   const decrease = () => {
     setAmount((oldAmount) => {
-      let tempAmount = oldAmount - 1;
+      let tempAmount = oldAmount - 1
       if (tempAmount < 1) {
         tempAmount = 1
       }
@@ -33,30 +34,38 @@ const AddToCart = ({ product }) => {
 
   return (
     <Wrapper>
-      <div className="colors">
-        <span>colors: </span>
-        <div>{
-          colors.map((color, index) => {
+      <div className='colors'>
+        <span>colors :</span>
+        <div>
+          {colors.map((color, index) => {
             return (
               <button
                 key={index}
                 style={{ background: color }}
-                className={`${mainColor === color ? 'color-btn active' : 'color-btn'}`}
-                onChange={() => setMainColor(color)}
+                className={`${mainColor === color ? 'color-btn active' : 'color-btn'
+                  }`}
+                onClick={() => setMainColor(color)}
               >
                 {mainColor === color ? <FaCheck /> : null}
               </button>
             )
-          })
-        }</div>
+          })}
+        </div>
       </div>
-      <div className="btn-container">
+      <div className='btn-container'>
         <AmountButtons
-          amount={amount}
           increase={increase}
           decrease={decrease}
+          amount={amount}
         />
-        <Link to='/cart' className='btn' onClick={() => addToCart(id, mainColor, amount, product)}>Add to cart</Link>
+
+        <Link
+          to='/cart'
+          className='btn'
+          onClick={() => addToCart(id, mainColor, amount, product)}
+        >
+          add to cart
+        </Link>
       </div>
     </Wrapper>
   )
