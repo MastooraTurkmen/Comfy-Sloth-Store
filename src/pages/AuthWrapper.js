fa8759d9798dd1aac0afcf648a8e1a1848dbce8a
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-15 10:55:40 +0430

    set up

--- src/pages/AuthWrapper.js
+++ src/pages/AuthWrapper.js
@@ -2,19 +2,23 @@ import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import styled from 'styled-components'
 
-const AuthWrapper = ({ children }) => {
+function AuthWrapper({ children }) {
   const { isLoading, error } = useAuth0()
 
   if (isLoading) {
-    return <Wrapper>
-      <h1>Loading...</h1>
-    </Wrapper>
+    return (
+      <Wrapper>
+        <h1>Loading....</h1>
+      </Wrapper>
+    )
   }
 
   if (error) {
-    return <Wrapper>
-      <h1>{error.message}</h1>
-    </Wrapper>
+    return (
+      <Wrapper>
+        <h1>{error.message}</h1>
+      </Wrapper>
+    )
   }
 
   return <>{children}</>
