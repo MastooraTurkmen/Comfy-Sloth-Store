643bc88ac9efd4dcc0a4dbb046f5173a6c80f2e4
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-15 10:54:34 +0430

    set up

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,22 +4,30 @@ import styled from 'styled-components'
 const Contact = () => {
   return (
     <Wrapper>
-      <div className="section-center">
+      <div className='section-center'>
         <h3>Join our newsletter and get 20% off</h3>
-        <div className="content">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Odit porro dignissimos aliquid perspiciatis nesciunt harum
-            sequi quos accusantium quam, expedita, corporis ad vero labore
-            officiis facere corrupti iste quas unde?</p>
+        <div className='content'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+            sint unde quaerat ratione soluta veniam provident adipisci cumque
+            eveniet tempore?
+          </p>
+          <form className='contact-form'>
+            <input
+              type='email'
+              className='form-input'
+              placeholder='enter email'
+            />
+            <button type='submit' className='submit-btn'>
+              subscribe
+            </button>
+          </form>
         </div>
-        <form action="https://formspree.io/f/xwkgaywy" method='POST' className="contact-form">
-          <input type="email" placeholder='enter email' name='_replyto' className="form-input" />
-          <button type="submit" className='submit-btn'>subscribe</button>
-        </form>
       </div>
     </Wrapper>
   )
 }
+
 const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
