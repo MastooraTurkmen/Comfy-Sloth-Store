405e95386df4c12fffe938d80609af71c1689b1d
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-15 10:54:44 +0430

    set up

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -7,125 +7,149 @@ import { FaCheck } from 'react-icons/fa'
 const Filters = () => {
   const {
     filters: {
-      text, category, company, color, price, max_price, shipping, min_price
+      text,
+      category,
+      company,
+      color,
+      min_price,
+      price,
+      max_price,
+      shipping,
     },
     updateFilters,
+    all_products,
     clearFilters,
-    all_products
-  } = useFilterContext();
+  } = useFilterContext()
 
   const categories = getUniqueValues(all_products, 'category')
   const companies = getUniqueValues(all_products, 'company')
-  const colors = getUniqueValues(all_products, 'color')
+  const colors = getUniqueValues(all_products, 'colors')
 
-  return <Wrapper>
-    <div className="content">
-      <form onSubmit={(e) => e.preventDefault()}>
-        {/* search input */}
-        <div className="form-control">
-          <input
-            type="text"
-            name="text"
-            placeholder='search'
-            value={text}
-            className='search-input'
-            onChange={updateFilters}
-          />
-        </div>
-        {/* end search input */}
-        {/* categories */}
-        <div className="form-control">
-          <h5>category</h5>
-          <div>{
-            categories.map((c, index) => {
-              return (
-                <button
-                  key={index}
-                  type='button'
-                  name='category'
-                  onClick={updateFilters}
-                  className={`${category === c.toLowerCase() ? 'active' : null}`}
-                >{c}</button>
-              )
-            })
-          }
+  return (
+    <Wrapper>
+      <div className='content'>
+        <form onSubmit={(e) => e.preventDefault()}>
+          {/* search input */}
+          <div className='form-control'>
+            <input
+              type='text'
+              name='text'
+              value={text}
+              placeholder='search'
+              onChange={updateFilters}
+              className='search-input'
+            />
           </div>
-        </div>
-        {/* end of categories */}
-        {/* companies */}
-        <div className="form-control">
-          <h5>company</h5>
-          <select name="company" value={company} onChange={updateFilters} className='company'>
-            {companies.map((c, index) => {
-              return <option key={index}>{c}</option>
-            })}
-          </select>
-        </div>
-        {/* end of companies */}
-        {/* colors */}
-        <div className="form-control">
-          <h5>colors</h5>
-          <div className="colors">
-            {colors.map((c, index) => {
-              if (c === 'all') {
+          {/* end of search input */}
+          {/* category */}
+          <div className='form-control'>
+            <h5>category</h5>
+            <div>
+              {categories.map((c, index) => {
+                return (
+                  <button
+                    key={index}
+                    onClick={updateFilters}
+                    type='button'
+                    name='category'
+                    className={`${category === c.toLowerCase() ? 'active' : null
+                      }`}
+                  >
+                    {c}
+                  </button>
+                )
+              })}
+            </div>
+          </div>
+          {/* end of category */}
+          {/* company */}
+          <div className='form-control'>
+            <h5>company</h5>
+            <select
+              name='company'
+              value={company}
+              onChange={updateFilters}
+              className='company'
+            >
+              {companies.map((c, index) => {
+                return (
+                  <option key={index} value={c}>
+                    {c}
+                  </option>
+                )
+              })}
+            </select>
+          </div>
+          {/* end of company */}
+          {/* colors */}
+          <div className='form-control'>
+            <h5>colors</h5>
+            <div className='colors'>
+              {colors.map((c, index) => {
+                if (c === 'all') {
+                  return (
+                    <button
+                      key={index}
+                      name='color'
+                      onClick={updateFilters}
+                      data-color='all'
+                      className={`${color === 'all' ? 'all-btn active' : 'all-btn'
+                        }`}
+                    >
+                      all
+                    </button>
+                  )
+                }
                 return (
                   <button
                     key={index}
                     name='color'
-                    className={`${color === 'all' ? 'all-btn active' : 'all-btn'}`}
-                    data-color='all'
+                    style={{ background: c }}
+                    className={`${color === c ? 'color-btn active' : 'color-btn'
+                      }`}
+                    data-color={c}
                     onClick={updateFilters}
                   >
-                    all
+                    {color === c ? <FaCheck /> : null}
                   </button>
                 )
-              }
-              return (
-                <button
-                  key={index}
-                  name='color'
-                  style={{ background: c }}
-                  className={`${color === c ? 'color-btn active' : 'active'}`}
-                  data-color={c}
-                  onClick={updateFilters}
-                >
-                  {color === c ? <FaCheck /> : null}
-                </button>
-              )
-            })}
+              })}
+            </div>
           </div>
-        </div>
-        {/* end of colors */}
-        {/* price */}
-        <div className="form-control">
-          <h5>price</h5>
-          <p className="price">{formatPrice(price)}</p>
-          <input
-            type="range"
-            name="price"
-            min={min_price}
-            max={max_price}
-            onChange={updateFilters}
-            value={price}
-          />
-        </div>
-        {/* end of price */}
-        {/* shipping */}
-        <div className="form-control shipping">
-          <label htmlFor="shipping">free shipping</label>
-          <input
-            type="checkbox"
-            name="shipping"
-            id="shipping"
-            onChange={updateFilters}
-            checked={shipping}
-          />
-        </div>
-        {/* end of shipping */}
-      </form>
-      <button type="button" className='clear-btn' onClick={clearFilters}>clear filters</button>
-    </div>
-  </Wrapper >
+          {/* end of colors */}
+          {/* price */}
+          <div className='form-control'>
+            <h5>price</h5>
+            <p className='price'>{formatPrice(price)}</p>
+            <input
+              type='range'
+              name='price'
+              onChange={updateFilters}
+              min={min_price}
+              max={max_price}
+              value={price}
+            />
+          </div>
+          {/* end of price */}
+          {/* shipping */}
+          <div className='form-control shipping'>
+            <label htmlFor='shipping'>free shipping</label>
+            <input
+              type='checkbox'
+              name='shipping'
+              id='shipping'
+              checked={shipping}
+              onChange={updateFilters}
+            />
+          </div>
+          {/* end of  shipping */}
+        </form>
+        <button type='button' className='clear-btn' onClick={clearFilters}>
+          clear filters
+        </button>
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
@@ -212,7 +236,6 @@ const Wrapper = styled.section`
     text-transform: capitalize;
     column-gap: 0.5rem;
     font-size: 1rem;
-    max-width: 200px;
   }
   .clear-btn {
     background: var(--clr-red-dark);
