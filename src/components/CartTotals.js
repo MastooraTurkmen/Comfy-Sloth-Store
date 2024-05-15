a326ec1105e78f3c86c390e445c5b07c266accb0
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-15 10:53:50 +0430

    set up

--- src/components/CartTotals.js
+++ src/components/CartTotals.js
@@ -13,16 +13,26 @@ const CartTotals = () => {
     <Wrapper>
       <div>
         <article>
-          <h5>subtotal : <span>{formatPrice(total_amount)}</span></h5>
-          <p>shipping fee: <span>{formatPrice(shipping_fee)}</span></p>
+          <h5>
+            subtotal :<span>{formatPrice(total_amount)}</span>
+          </h5>
+          <p>
+            shipping fee :<span>{formatPrice(shipping_fee)}</span>
+          </p>
           <hr />
-          <h4>order total : <span>{formatPrice(total_amount + shipping_fee)}</span></h4>
+          <h4>
+            order total :<span>{formatPrice(total_amount + shipping_fee)}</span>
+          </h4>
         </article>
         {myUser ? (
           <Link to='/checkout' className='btn'>
             proceed to checkout
           </Link>
-        ) : (<button onClick={loginWithRedirect}>login</button>)}
+        ) : (
+          <button onClick={loginWithRedirect} className='btn'>
+            login
+          </button>
+        )}
       </div>
     </Wrapper>
   )
