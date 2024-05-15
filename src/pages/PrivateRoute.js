1a44bc89e2d07e0f5bafaeec2c93ad80ccd6029f
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-15 10:56:08 +0430

    set up

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -3,10 +3,10 @@ import { Navigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useAuth0()
+  const { user } = useAuth0();
 
   if (!user) {
-    return <Navigate to='/' />
+    return <Navigate to='/' />;
   }
   return children;
 };
