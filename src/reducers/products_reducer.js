e5dd56211bd9c2ce1f83d56d95888cdc99596bdc
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-15 10:56:23 +0430

    set up

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -11,34 +11,49 @@ import {
 
 const products_reducer = (state, action) => {
   if (action.type === SIDEBAR_OPEN) {
-    return { ...state, isOpenSidebar: true }
+    return { ...state, isSidebarOpen: true }
   }
   if (action.type === SIDEBAR_CLOSE) {
-    return { ...state, isOpenSidebar: false }
+    return { ...state, isSidebarOpen: false }
   }
+
   if (action.type === GET_PRODUCTS_BEGIN) {
     return { ...state, products_loading: true }
   }
   if (action.type === GET_PRODUCTS_SUCCESS) {
-    const featured_products = action.payload.filter((product) => product.featured === true)
+    const featured_products = action.payload.filter(
+      (product) => product.featured === true
+    )
     return {
       ...state,
-      products: action.payload,
       products_loading: false,
-      featured_products
+      products: action.payload,
+      featured_products,
     }
   }
   if (action.type === GET_PRODUCTS_ERROR) {
-    return { ...state, products_error: true, products_loading: false }
+    return { ...state, products_loading: false, products_error: true }
   }
   if (action.type === GET_SINGLE_PRODUCT_BEGIN) {
-    return { ...state, single_product_loading: true, single_product_error: false }
+    return {
+      ...state,
+      single_product_loading: true,
+      single_product_error: false,
+    }
   }
   if (action.type === GET_SINGLE_PRODUCT_SUCCESS) {
-    return { ...state, single_product_loading: false, single_product: action.payload }
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product: action.payload,
+    }
   }
   if (action.type === GET_SINGLE_PRODUCT_ERROR) {
-    return { ...state, single_product_error: true, single_product_loading: false }
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product_error: true,
+    }
   }
   throw new Error(`No Matching "${action.type}" - action type`)
 }
