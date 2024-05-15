972c77df89ecdfe48a9308d81cb13d79cff84377
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-15 09:59:49 +0430

    set up

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -7,9 +7,25 @@ const stripe = require('stripe')(REACT_APP_STRIPE_SECRET_KEY)
 exports.handler = async function (event, context) {
     if (event.body) {
         const { cart, shipping_fee, total_amount } = JSON.parse(event.body)
-        return {
-            statusCode: 200,
-            body: JSON.stringify(cart),
+
+        const calculateOrderAmount = () => {
+            return shipping_fee + total_amount
+        }
+
+        try {
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: calculateOrderAmount(),
+                currency: 'usd',
+            })
+            return ({
+                statusCode: 200,
+                body: JSON.stringify({ clientSecrect: paymentIntent.client_secret })
+            })
+        } catch (error) {
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ error: error.message }),
+            }
         }
     }
     return {
