93805aa19145411cbc7b31fb5925b057d61dce26
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-15 10:55:27 +0430

    set up

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -9,8 +9,8 @@ import {
 } from '../actions'
 
 const getLocalStorage = () => {
-  let cart = localStorage.getItem('cart');
-  if (cart) {
+  let cart = localStorage.getItem('cart')
+  if (cart && cart !== 'undefined') {
     return JSON.parse(localStorage.getItem('cart'))
   } else {
     return []
@@ -28,34 +28,38 @@ const CartContext = React.createContext()
 
 export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
-
   // add to cart
   const addToCart = (id, color, amount, product) => {
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } })
   }
-
   // remove item
   const removeItem = (id) => {
     dispatch({ type: REMOVE_CART_ITEM, payload: id })
   }
-
   // toggle amount
   const toggleAmount = (id, value) => {
-    dispatch({ type: TOGGLE_CART_ITEM_AMOUNT, payload: { id, value } })
+    dispatch({
+      type: TOGGLE_CART_ITEM_AMOUNT,
+      payload: {
+        id,
+        value,
+      },
+    })
   }
-
   // clear cart
-  const clearCart = () => { }
-
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART })
+  }
   useEffect(() => {
     dispatch({ type: COUNT_CART_TOTALS })
     localStorage.setItem('cart', JSON.stringify(state.cart))
   }, [state.cart])
-
   return (
-    <CartContext.Provider value={{
-      ...state, addToCart, removeItem, toggleAmount, clearCart
-    }}>{children}</CartContext.Provider>
+    <CartContext.Provider
+      value={{ ...state, addToCart, removeItem, toggleAmount, clearCart }}
+    >
+      {children}
+    </CartContext.Provider>
   )
 }
 // make sure use
