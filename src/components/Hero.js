2a83e459be531ab47996779e20de25372db783e8
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-15 10:54:50 +0430

    set up

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,17 +7,24 @@ import heroBcg2 from '../assets/hero-bcg-2.jpeg'
 const Hero = () => {
   return (
     <Wrapper className='section-center'>
-      <article className="content">
-        <h1>design your<br /> comfort zone</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Distinctio culpa ea quisquam atque ipsa! Eius perspiciatis ratione beatae
-          ut vero consectetur praesentium similique quos excepturi error, adipisci, hic, voluptates tenetur.
+      <article className='content'>
+        <h1>
+          design your <br />
+          comfort zone
+        </h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, at
+          sed omnis corporis doloremque possimus velit! Repudiandae nisi odit,
+          aperiam odio ducimus, obcaecati libero et quia tempora excepturi quis
+          alias?
         </p>
-        <Link to='/products' className='btn hero-btn'>shop now</Link>
+        <Link to='/products' className='btn hero-btn'>
+          shop now
+        </Link>
       </article>
-      <article className="img-container">
-        <img src={heroBcg} alt="nice table" className='main-img' />
-        <img src={heroBcg2} alt="person working" className='accent-img' />
+      <article className='img-container'>
+        <img src={heroBcg} alt='nice table' className='main-img' />
+        <img src={heroBcg2} alt='person working' className='accent-img' />
       </article>
     </Wrapper>
   )
