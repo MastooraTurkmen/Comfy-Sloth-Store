427aa0516c9b0fa1d677b3e276c672610d793c18
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-15 10:55:38 +0430

    set up

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -8,21 +8,23 @@ const AboutPage = () => {
     <main>
       <PageHero title='about' />
       <Wrapper className='page section section-center'>
-        <img src={aboutImg} alt="nice desk" />
+        <img src={aboutImg} alt='nice desk' />
         <article>
-          <div className="title">
+          <div className='title'>
             <h2>our story</h2>
-            <div className="underline"></div>
+            <div className='underline'></div>
           </div>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Ut earum, odit laboriosam quidem est sapiente optio esse saepe!
-            Atque dolores fugiat reprehenderit architecto. Odit error ex atque hic suscipit labore!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Ut earum, odit laboriosam quidem est sapiente optio esse saepe!
-            Atque dolores fugiat reprehenderit architecto. Odit error ex atque hic suscipit labore!
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Ut earum, odit laboriosam quidem est sapiente optio esse saepe!
-            Atque dolores fugiat reprehenderit architecto. Odit error ex atque hic suscipit labore!
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat
+            accusantium sapiente tempora sed dolore esse deserunt eaque
+            excepturi, delectus error accusamus vel eligendi, omnis beatae.
+            Quisquam, dicta. Eos quod quisquam esse recusandae vitae neque
+            dolore, obcaecati incidunt sequi blanditiis est exercitationem
+            molestiae delectus saepe odio eligendi modi porro eaque in libero
+            minus unde sapiente consectetur architecto. Ullam rerum, nemo iste
+            ex, eaque perspiciatis nisi, eum totam velit saepe sed quos
+            similique amet. Ex, voluptate accusamus nesciunt totam vitae esse
+            iste.
           </p>
         </article>
       </Wrapper>
