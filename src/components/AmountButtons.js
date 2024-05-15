cee9bed3abaaa82dfb10d32e5fe5b19dcaa95287
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-15 10:53:32 +0430

    set up

--- src/components/AmountButtons.js
+++ src/components/AmountButtons.js
@@ -4,15 +4,15 @@ import { FaPlus, FaMinus } from 'react-icons/fa'
 
 const AmountButtons = ({ increase, decrease, amount }) => {
   return (
-    < Wrapper className='amount-btns'>
-      <button type='button' onClick={decrease} className="amount-btn">
+    <Wrapper className='amount-btns'>
+      <button type='button' className='amount-btn' onClick={decrease}>
         <FaMinus />
       </button>
-      <h2 className="amount">{amount}</h2>
-      <button type='button' onClick={increase} className="amount-btn">
+      <h2 className='amount'>{amount}</h2>
+      <button type='button' className='amount-btn' onClick={increase}>
         <FaPlus />
       </button>
-    </Wrapper >
+    </Wrapper>
   )
 }
 
